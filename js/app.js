1355e349a96fa1dc839a80f3d0abd636c18be11a
woshinivek <woshinivek@yahoo.com>
2022-04-20 17:39:42 +0300

    changes

--- js/app.js
+++ js/app.js
@@ -1,3 +1,11 @@
 import galleryItems from "./galleryItems.js";
 
-console.log(galleryItems);
+const galleryRef = document.querySelector(".js-gallery");
+
+const galleryMarkup = galleryItems
+  .map((item) => {
+    return `<li><div class="thumb"><img src=${item.preview} width="340"  ></div></li>`;
+  })
+  .join("");
+
+galleryRef.innerHTML = galleryMarkup;
